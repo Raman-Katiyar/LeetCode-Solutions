40c2aeeabce4d579d8da183a08fa06cfa49b1ce1
Raman Katiyar <ramankatiyar273@gmail.com>
2026-07-13 16:24:43 +0530

    Time: 6 ms (85.71%), Space: 46.4 MB (70.77%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    public class Pair {
+        int node;
+        int cost;
+        Pair(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+    
+    public class Triplet {
+        int node;
+        int cost;
+        int stops;
+        // FIX 1: Parameter name matched correctly
+        Triplet(int node, int cost, int stops) {
+            this.node = node;
+            this.cost = cost;
+            this.stops = stops;
+        }
+    }
+    
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        for(int i=0; i<flights.length; i++){
+            int from = flights[i][0], to = flights[i][1], price = flights[i][2];
+            adj.get(from).add(new Pair(to, price));
+        }
+
+        int[] ans = new int[n];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[src] = 0;
+        
+        Queue<Triplet> pq = new LinkedList<>();
+        pq.add(new Triplet(src, 0, 0));
+        
+        while(pq.size() > 0) {
+            Triplet top = pq.remove();
+            int node = top.node, cost = top.cost, stops = top.stops;
+            
+            // Agar stops limit cross ho gayi hai, toh aage explore mat karo
+            if(stops > k) continue; 
+            
+            for(Pair p : adj.get(node)) {
+                int totalCost = cost + p.cost;
+                
+                // Sasta raasta mila toh update karo aur queue mein daalo
+                if(totalCost < ans[p.node]) {
+                    ans[p.node] = totalCost;
+                    // FIX 2 & 3: stops + 1 kiya hai agle step ke liye
+                    pq.add(new Triplet(p.node, totalCost, stops + 1));
+                }
+            }
+        }
+        
+        if(ans[dst] == Integer.MAX_VALUE) return -1;
+        return ans[dst];
+    }
+}
