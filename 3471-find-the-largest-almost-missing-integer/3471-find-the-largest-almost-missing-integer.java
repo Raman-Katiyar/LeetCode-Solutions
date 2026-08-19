e3f9b7b8bcad9cbcb5c1dc4180bdf3b2a1d0d965
Raman Katiyar <ramankatiyar273@gmail.com>
2026-08-19 08:43:50 +0530

    Time: 6 ms (38.25%), Space: 46.4 MB (32.63%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        
+        Map<Integer, Integer> subarrayCount = new HashMap<>();
+        for (int i = 0; i <= n - k; i++) {
+            Set<Integer> uniqueInSubarray = new HashSet<>();
+            
+            for (int j = i; j < i + k; j++) {
+                uniqueInSubarray.add(nums[j]);
+            }
+
+            for (int num : uniqueInSubarray) {
+                subarrayCount.put(num, subarrayCount.getOrDefault(num, 0) + 1);
+            }
+        }
+        int maxVal = -1;
+
+        for (Map.Entry<Integer, Integer> entry : subarrayCount.entrySet()) {
+            int element = entry.getKey();
+            int count = entry.getValue();
+            if (count == 1) {
+                maxVal = Math.max(maxVal, element);
+            }
+        }
+        return maxVal;
+    }
+}
